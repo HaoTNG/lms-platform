ba8029901b1ca7be230c1f3bbf15c4ebc93c332d
HaoTNG <nguyentronghao13579@gmail.com>
2026-02-25 19:04:08 +0700

    fixing

--- target/generated-sources/annotations/com/example/lms/mapper/ReportTicketMapperImpl.java
+++ target/generated-sources/annotations/com/example/lms/mapper/ReportTicketMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.9 (Arch Linux)"
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.45.0.v20260128-0750, environment: Java 21.0.9 (Eclipse Adoptium)"
 )
 @Component
 public class ReportTicketMapperImpl implements ReportTicketMapper {
@@ -26,12 +26,12 @@ public class ReportTicketMapperImpl implements ReportTicketMapper {
         if ( reportTicket.getStatus() != null ) {
             reportTicketDTO.setStatus( reportTicket.getStatus().name() );
         }
-        reportTicketDTO.setId( reportTicket.getId() );
-        reportTicketDTO.setTitle( reportTicket.getTitle() );
-        reportTicketDTO.setDescription( reportTicket.getDescription() );
         reportTicketDTO.setAdminResponse( reportTicket.getAdminResponse() );
         reportTicketDTO.setCreatedAt( reportTicket.getCreatedAt() );
+        reportTicketDTO.setDescription( reportTicket.getDescription() );
+        reportTicketDTO.setId( reportTicket.getId() );
         reportTicketDTO.setResolvedAt( reportTicket.getResolvedAt() );
+        reportTicketDTO.setTitle( reportTicket.getTitle() );
 
         return reportTicketDTO;
     }
@@ -48,12 +48,12 @@ public class ReportTicketMapperImpl implements ReportTicketMapper {
         if ( dto.getStatus() != null ) {
             reportTicket.status( Enum.valueOf( ReportTicketStatus.class, dto.getStatus() ) );
         }
-        reportTicket.id( dto.getId() );
-        reportTicket.title( dto.getTitle() );
-        reportTicket.description( dto.getDescription() );
         reportTicket.adminResponse( dto.getAdminResponse() );
         reportTicket.createdAt( dto.getCreatedAt() );
+        reportTicket.description( dto.getDescription() );
+        reportTicket.id( dto.getId() );
         reportTicket.resolvedAt( dto.getResolvedAt() );
+        reportTicket.title( dto.getTitle() );
 
         return reportTicket.build();
     }
